eef12f8f995a8f3f617ae8fe35753e82c7ba74ea
romanazaruk <canspro1105@gmail.com>
2020-05-11 14:20:51 +0300

    Used destructuring in props

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 
-export default function Book({data}) {
+export default function Book({ data }) {
+  const { imageLinks, title, publisher, canonicalVolumeLink } = data;
   return (
     <div className="bookForm">
-      <img
-        src={data.imageLinks.thumbnail}
-        alt="bookTitle"
-        className="bookImg"
-      />
+      <img src={imageLinks.thumbnail} alt="bookTitle" className="bookImg" />
       <div className="bookInfo">
-        <h4 className="bookTitle">Title: {data.title} </h4>
-        <h4 className="bookPublisher">
-          Publisher: {data.publisher}
-        </h4>
+        <h4 className="bookTitle">Title: {title} </h4>
+        <h4 className="bookPublisher">Publisher: {publisher}</h4>
         <h4>
-          <a
-            href={data.canonicalVolumeLink}
-            className="bookUrl"
-          >
+          <a href={canonicalVolumeLink} className="bookUrl">
             Go to book
           </a>
         </h4>
